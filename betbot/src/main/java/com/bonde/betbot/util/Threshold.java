6fbab8cf0a8999489f2c55dde2d753bf69890554
f12defilippis <francesco.defilippis@gmail.com>
2017-06-24 16:17:51 +0200

    modify threshold

--- betbot/src/main/java/com/bonde/betbot/util/Threshold.java
+++ betbot/src/main/java/com/bonde/betbot/util/Threshold.java
@@ -2,8 +2,8 @@ package com.bonde.betbot.util;
 
 public interface Threshold {
 
-	public static Integer MINIMUM_EVENTS = 20;
-	public static Integer DAYS_BEFORE = 150;
+	public static Integer MINIMUM_EVENTS = 15;
+	public static Integer DAYS_BEFORE = 180;
 	public static Integer SUMMARY_FREQUENCY = 14;
 
 	public static Integer MINIMUM_SUMMARIES = 2;
